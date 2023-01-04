ad316f0753e33741e9d0e8bd00df38b7e6f0f5a7
Meenakshi Suresh <119519427+MnkshiSuresh@users.noreply.github.com>
2023-01-04 08:53:59 +0530

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Palindrome {
+public static void main(String[] args) {
+Scanner input =new Scanner(System.in);
+System.out.println("Enter the string: ");
+String str;
+str=input.nextLine();
+int n=str.length();
+int i;
+String str2="";
+for(i=n-1;i>=0;i--){
+str2+=str.charAt(i);
+}
+System.out.println(str2);
+if(str.equals(str2)){
+System.out.println("Is Palindrome");
+}
+else{
+System.out.println("Not Palindrome");
+}
+}
+}
