384aefad77c2b6f954144135c1abbe0ef2cb77c5
Meenakshi Suresh <119519427+MnkshiSuresh@users.noreply.github.com>
2023-02-17 18:30:09 +0530

    Update and rename palindrome2.java to Palindrome2.java

--- Palindrome2.java
+++ Palindrome2.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class palindrome2{
+public class Palindrome2{
     public static void main(String[] args){
         Scanner s=new Scanner(System.in);
         System.out.println("Enter the string:");
