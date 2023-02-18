fe6d20c6c6a4330d9beefef74cb47c638877dc54
Meenakshi Suresh <119519427+MnkshiSuresh@users.noreply.github.com>
2023-02-18 11:11:02 +0530

    Update CreationOfFile.java

--- CreationOfFile.java
+++ CreationOfFile.java
@@ -2,6 +2,6 @@ import java.io.File;
 public class FileHandling{
     public static void main(String[] args){
         File obj=new File("myFile.txt");
-        System.out.println("File created!");
+        System.out.println("File created! with name "+obj.getName());
     }
 }
